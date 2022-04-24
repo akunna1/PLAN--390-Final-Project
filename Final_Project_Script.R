9e5e9ff9a234c9d2dcfad8e2edf66873a44b19b3
Amarachi Akunna <akunna1@live.unc.edu>
2022-04-24 18:35:34 -0400

    24th April 2022

--- Final_Project_Script.R
+++ Final_Project_Script.R
@@ -1,6 +1,4 @@
-# Setting my working directory
-setwd("C:/Users/akunna1/Desktop/PLAN 390/Final Project/Files Used")
-
+# Set working directory to Source File Location
 
 # Load libraries
 library(tidyverse)  # dealing with tabular data
@@ -13,47 +11,109 @@ library(scales)# graphical scaling
 
 # Importing my .csv data to the Global Environment
 # 3 years of data
-police_data_2020 = read_csv("2020_police_data.csv")
-police_data_2021 = read_csv("2021_police_data.csv")
-police_data_2022 = read_csv("2022_police_data.csv")
+arrest_data_2018 = read_csv("2018_police_data.csv")
+arrest_data_2019 = read_csv("2019_police_data.csv")
+arrest_data_2020 = read_csv("2020_police_data.csv")
 
-police_data <- rbind(police_data_2020,police_data_2021,police_data_2022)
+arrest_data <- rbind(arrest_data_2018,arrest_data_2019,arrest_data_2020)
 
-#Data Analysis
-#police_data$reported_date <- as.POSIXct(police_data$reported_date, format = "%Y/%m/%d %H:%M:%S")
+# **********************
+# Analysis of Year 2018 Data
+# ** Run Each Month Analysis at a time to see each plot**
+# Month Analysis
+#extracting month from date and making a new column for it
+arrest_data_2018$month_of_arrest <- format(as.POSIXct(arrest_data_2018$Date_of_Arrest, format = "%Y/%m/%d %H:%M:%S"), format = "%m")
 
-#extracting time from date time
-police_data$reported_time <- format(as.POSIXct(police_data$reported_date, format = "%Y/%m/%d %H:%M:%S"), format = "%H:%M:%S")
-police_data$reported_time # includes hours, mins and secs
+# changing months to factors
+arrest_data_2018$month_of_arrest <- as.factor(arrest_data_2018$month_of_arrest)
 
-# changing hour factors
-police_data$reported_hour <- as.factor(police_data$reported_hour)
-
-# plotting the incidents by hours in a day
-hour_data <- police_data %>%
-  group_by(reported_hour) %>%
-  summarise(Total = n()) # grouping the data with hour and count
+# plotting the arrests in 2018 by months
+month_data <- arrest_data_2018 %>%
+  group_by(month_of_arrest) %>%
+  summarise(Total = n()) # grouping the data with month and count
 
 #see in tabular form
-data.table(hour_data)
+data.table(month_data)
 
 # Visualize the data
-ggplot(hour_data, aes(reported_hour,Total)) +
-  geom_bar(stat = "identity", fill = "orange", color = "black") +
-  ggtitle("Incidents By Hour") +
+ggplot(month_data, aes(month_of_arrest,Total)) +
+  geom_bar(stat = "identity", fill = "skyblue", color = "black") +
+  ggtitle("Year 2018 Arrests By Month") +
   theme(legend.position = "none") +
   scale_y_continuous(labels = comma)
 
-month_hour_data <- police_data %>%
-  group_by(reported_month,reported_hour) %>%
-  summarise(Total = n()) # grouping the data with hour and count
+# **********************
+# Analysis of Year 2019 Data
+# Month Analysis
+#extracting month from date and making a new column for it
+arrest_data_2019$month_of_arrest <- format(as.POSIXct(arrest_data_2019$Date_of_Arrest, format = "%Y/%m/%d %H:%M:%S"), format = "%m")
+
+# changing months to factors
+arrest_data_2019$month_of_arrest <- as.factor(arrest_data_2019$month_of_arrest)
+
+# plotting the arrests in 2019 by months
+month_data <- arrest_data_2019 %>%
+  group_by(month_of_arrest) %>%
+  summarise(Total = n()) # grouping the data with month and count
 
 #see in tabular form
-data.table(month_hour_data)
+data.table(month_data)
 
-# plot the same
-ggplot(month_hour_data, aes(reported_hour,Total, fill=reported_month)) +
-  geom_bar(stat = "identity") +
-  ggtitle("Incidents By Hour and Month") +
+# Visualize the data
+ggplot(month_data, aes(month_of_arrest,Total)) +
+  geom_bar(stat = "identity", fill = "pink", color = "black") +
+  ggtitle("Year 2019 Arrests By Month") +
+  theme(legend.position = "none") +
   scale_y_continuous(labels = comma)
-  
+
+# **********************
+# Analysis of Year 2020 Data
+# Month Analysis
+#extracting month from date and making a new column for it
+arrest_data_2020$month_of_arrest <- format(as.POSIXct(arrest_data_2020$Date_of_Arrest, format = "%Y/%m/%d %H:%M:%S"), format = "%m")
+
+# changing months to factors
+arrest_data_2020$month_of_arrest <- as.factor(arrest_data_2020$month_of_arrest)
+
+# plotting the arrests in 2020 by months
+month_data <- arrest_data_2020 %>%
+  group_by(month_of_arrest) %>%
+  summarise(Total = n()) # grouping the data with month and count
+
+#see in tabular form
+data.table(month_data)
+
+# Visualize the data
+ggplot(month_data, aes(month_of_arrest,Total)) +
+  geom_bar(stat = "identity", fill = "wheat2", color = "black") +
+  ggtitle("Year 2020 Arrests By Month") +
+  theme(legend.position = "none") +
+  scale_y_continuous(labels = comma)
+
+# **********************
+# Analysis of 2018 -2020 Data
+# Month Analysis
+#extracting month from date and making a new column for it
+arrest_data$month_of_arrest <- format(as.POSIXct(arrest_data$Date_of_Arrest, format = "%Y/%m/%d %H:%M:%S"), format = "%m")
+
+# changing months to factors
+arrest_data$month_of_arrest <- as.factor(arrest_data$month_of_arrest)
+
+# plotting the arrests in 2018 - 2020 by months
+month_data <- arrest_data %>%
+  group_by(month_of_arrest) %>%
+  summarise(Total = n()) # grouping the data with month and count
+
+#see in tabular form
+data.table(month_data)
+
+# Visualize the data
+ggplot(month_data, aes(month_of_arrest,Total)) +
+  geom_bar(stat = "identity", fill = "thistle1", color = "black") +
+  ggtitle("Year 2018-2020 Arrests By Month") +
+  theme(legend.position = "none") +
+  scale_y_continuous(labels = comma)
+
+
+
+
